69eb3a64706f94190f76722b8ee3ba6daae82163
mohammad issa <mohammad.issa@optimumpartners.com>
2018-04-15 11:20:50 +0300

    events schema

--- inAmman/imports/api/events/eventsCategory.js
+++ inAmman/imports/api/events/eventsCategory.js
@@ -7,31 +7,36 @@ if (Meteor.isServer) {
 	Meteor.publish('events_category', function eventsCategoryPublication() {
 		return EventsCategory.find();
 	});
-// Meteor.methods({
-//   'events.addCategory'(obj) {
-//     try {
-// 	    check(text, String);
-//   	}
-//   	catch (error) {
-//       throw new Meteor.Error('text is not string');
-//   	}
-//     // Make sure the user is logged in before inserting a task
-//     if(!Meteor.userId() && !Meteor.user().roles.indexOf('Admin') !== -1){
-//       throw new Meteor.Error('not-authorized');
-//     }
- 
-//     EventsCategory.insert({
-//       text,
-//       createdAt: new Date(),
-//       owner: this.userId,
-//       username: Meteor.users.findOne(this.userId).username,
-//     });
-//   },
 
-//   'events.removeEvent'(evtId) {
-//     check(evtId, String);
-//     Events.remove(evtId);
-// 	}
-// });
+	Meteor.methods({
+		'events.addCategory'(category) {
+			try {
+				check(category, {
+					name: String,
+				});
+			}
+			catch (error) {
+			  throw new Meteor.Error('text is not string');
+			}
+
+			// check if category is already exists
+			if(EventsCategory.find({name:category.name}).fetch().length !== 0) {
+			  throw new Meteor.Error('category name is already exists');
+			}
+
+			// Make sure the user is logged in before inserting a task
+			if(!Meteor.userId() && !Meteor.user().roles.indexOf('Admin') !== -1){
+			  throw new Meteor.Error('not-authorized');
+			}
+	 
+			EventsCategory.insert({
+				name: category.name,
+				active: category.active || true,
+				visible: category.visible || true,
+				createdAt: new Date()
+			});
+		},
+
+	});
 
 }
